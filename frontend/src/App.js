a36c5eee84cea9aea6ee62dee67d0d98972dd4c5
udeshika <udeshikamadubhashini95@gmail.com>
2025-08-10 18:38:36 +1000

    FB-19 Ui/Ux adjusments completed
    -added home pages for client and freelancer -added user type selection drop down

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
+import Home from './pages/freelancer/FreelancerHome';
+import ClientHome from './pages/client/ClientHome';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/freelancer_home" element={<Home />} />
+        <Route path="/client_home" element={<ClientHome />} />
       </Routes>
     </Router>
   );
