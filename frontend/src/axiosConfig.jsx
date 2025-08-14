3a23cc48bd766626e56e2367351e583dbd5a6fd9
udeshika <udeshikamadubhashini95@gmail.com>
2025-08-15 08:33:59 +1000

    Modified the server ip

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // live
+  baseURL: 'http://16.176.5.103:5001', // live
   //baseURL: 'http://3.26.96.188:5001', // test
   headers: { 'Content-Type': 'application/json' },
 });
